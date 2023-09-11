91973cd330f2d5f7f315545dd8b1fed347c745e6
duongnguyen <daiduongnguyen2709@gmail.com>
2023-09-11 17:30:29 +0700

    [FIX] openupgrade framework: correct translation againnnnn

--- openupgrade_framework/odoo_patch/odoo/fields.py
+++ openupgrade_framework/odoo_patch/odoo/fields.py
@@ -22,6 +22,10 @@ def _convert_db_column(self, model, column):
                 'ELSE t.value || m."%s" END' % self.name,
                 'ELSE m."%s" || t.value END' % self.name,
             )
+            query = query.replace(
+                'AND it.state = "translated"',
+                "",
+            ).replace('AND state = "translated"', "")
             openupgrade.logged_query(model._cr, query)
 
 
