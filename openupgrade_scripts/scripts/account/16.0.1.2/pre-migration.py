2f42696556b98ed4ea95e416b3da4c2e3dfa944e
duongnguyen <daiduongnguyen2709@gmail.com>
2023-09-22 15:23:34 +0700

    [IMP] analytic_distribution*: only generate analytic distribution when its analytic tag is check for analytic distribution

--- openupgrade_scripts/scripts/account/16.0.1.2/pre-migration.py
+++ openupgrade_scripts/scripts/account/16.0.1.2/pre-migration.py
@@ -447,6 +447,9 @@ def _aml_fast_fill_analytic_distribution(env):
                         ON tag_rel.account_move_line_id = move_line.id
                     JOIN account_analytic_distribution dist
                         ON dist.tag_id = tag_rel.account_analytic_tag_id
+                    JOIN account_analytic_tag aat
+                        ON aat.id = tag_rel.account_analytic_tag_id
+                    WHERE aat.active_analytic_distribution = true
                 ) AS all_line_data
                 GROUP BY all_line_data.move_line_id, all_line_data.analytic_account_id
             )
