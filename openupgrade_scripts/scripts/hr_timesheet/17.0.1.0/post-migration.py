0a5fe3a49961e53ea0cd5049e389f3fef68a8362
Roy Le <43790414+royle-viindoo@users.noreply.github.com>
2024-05-28 09:49:19 +0700

    Update openupgrade_scripts/scripts/hr_timesheet/17.0.1.0/post-migration.py

--- openupgrade_scripts/scripts/hr_timesheet/17.0.1.0/post-migration.py
+++ openupgrade_scripts/scripts/hr_timesheet/17.0.1.0/post-migration.py
@@ -5,10 +5,9 @@ def _project_update_fill_timesheet(env):
     updates = env["project.update"].with_context(active_test=False).search([])
     for update in updates:
         project = update.project_id
-        company = project.company_id
-        if not project.company_id.timesheet_encode_uom_id:
-            continue
         encode_uom = company.timesheet_encode_uom_id
+        if not encode_uom:
+            continue
         ratio = env.ref("uom.product_uom_hour").ratio / encode_uom.ratio
         update.write(
             {
